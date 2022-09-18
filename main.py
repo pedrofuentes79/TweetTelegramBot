259aa9569f8098aabd938e0b69e4a94694edc577
pedrofuentes79 <pedrofuentes7799@gmail.com>
2022-09-18 15:16:16 -0300

    Renamed and fixed timezone problems

--- main.py
+++ main.py
@@ -9,12 +9,9 @@ from jsonFileManagement import get_msg_ids, save_message_id, delete_content_from
 
 
 chat_ids = ["5586625183"]
-twitter_accounts = ["AlertaNews24", "porquetendencia"]
-
+twitter_accounts = ["porquetendencia", "AlertaNews24"]
 bearer_token = os.environ.get("BEARER_TOKEN")
-search_url = "https://api.twitter.com/2/tweets/search/recent"
-
-yesterday_date = (datetime.datetime.utcnow() - datetime.timedelta(days=1))
+yesterday_date = (datetime.datetime.utcnow() - datetime.timedelta(hours=24))
 
 # Optional params: start_time,end_time,since_id,until_id,max_results,next_token,
 # expansions,tweet.fields,media.fields,poll.fields,place.fields,user.fields
@@ -29,9 +26,10 @@ def bearer_oauth(r):
     return r
 
 
-def connect_to_endpoint(url, params):
+def connect_to_endpoint(search_url, params):
     response = requests.request("GET", search_url, auth=bearer_oauth, params=params)
     print(response.status_code)
+    print(f"Tweets scraped succesfully")
     if response.status_code != 200:
         raise Exception(response.status_code, response.text)
     return response.json()
@@ -39,33 +37,39 @@ def connect_to_endpoint(url, params):
 
 def main():
     #deletes previous messages in each chat
+    
     for chat_id in chat_ids:
         msg_id_list = get_msg_ids(chat_id=chat_id)
         if msg_id_list is not []:
             for msg in msg_id_list:
                 delete_messages(msg, chat_id=chat_id)
         delete_content_from_chat_id(chat_id=chat_id)  
+    
     #For each profile, sends to each chat the last twenty tweets 
     #only if the tweet was published in the last 24hs
     for profile in twitter_accounts:
         for chat_id in chat_ids:
-            #connect to endpoint
+            #Get the tweets from the account
             query_params = {'query': '(from:'+ profile + ')','tweet.fields': 'created_at'}
-            json_response = connect_to_endpoint(search_url, query_params)["data"]
-            
+            json_response = connect_to_endpoint("https://api.twitter.com/2/tweets/search/recent", 
+                                                query_params)["data"]
 
-            #iterates through the tweets in a reversed manner so that the "oldest" ones are sent first
+            #Iterates through the tweets in a reversed manner so that the oldest ones are sent first
             for tweet in reversed(json_response):
-                #get the tweet date and parse it to a dt object
+                #Get the tweet date and parse it to a dt object
                 tweet_date_str = tweet["created_at"]
                 tweet_date = parse(tweet_date_str, yearfirst=True)
                 
-                #change the timezone to argentina's and format it
+                #Creates new variable to store the tweet date with local timezone, for display purposes
                 local_tz_tweet_date_str = tweet_date.astimezone(pytz.timezone("America/Argentina/Buenos_Aires"))
                 local_tz_tweet_date_str = local_tz_tweet_date_str.strftime("%d/%m %H:%M")
 
-                #if the tweet has been posted 24hs prior to the execution of this script, send it
-                if ( tweet_date.timestamp() > yesterday_date.timestamp() ):
+                #Removes timezone data from tweet_date in order for it to be comparable with yesterday_date
+                #This is functional because they both are in utc.
+                tweet_date = tweet_date.replace(tzinfo=None)
+
+                #If the tweet has been posted 24hs prior to the execution of this script, send it
+                if ( tweet_date > yesterday_date):
                     response, msg_id = send_message(txt=tweet["text"], 
                                                     date=local_tz_tweet_date_str, chat_id=chat_id)
                     if response['ok']:
--- send_message.py
+++ send_message.py
@@ -1,14 +1,12 @@
 import os
 import requests
-import json
 
 telegram_token = os.environ.get("TELEGRAM_API_KEY")
-
 
 def send_message(txt, date, chat_id):
     #return to this msg after testing
     #msg = date + "\n" + txt
-    msg = txt
+    msg = date + "\n" + txt
     url = 'https://api.telegram.org/bot' + telegram_token + '/sendMessage?chat_id=' + chat_id + '&parse_mode=Markdown&text=' + msg
     url = url.replace("#", "%23")
 
